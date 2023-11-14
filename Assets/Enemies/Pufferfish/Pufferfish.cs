588f78637e1df6fe056795312616a614f3f6776b
Kenneth Mitchell Burt <kmb898@psu.edu>
2023-11-13 20:00:00 -0500

    Player Detector

---
+++ Assets/Enemies/Pufferfish/Pufferfish.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Basic Pufferfish enemy script
+public class Pufferfish : MonoBehaviour
+{
+    //Attributes
+    public Transform anchor;
+    public float radius;
+
+
+    //Objects
+    GameObject CrabDad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
